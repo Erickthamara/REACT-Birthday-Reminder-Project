93f5bae879a1f89ed33eb7865a97bdc081872643
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2024-01-06 17:27:28 +0300

    Almost cpmlete with tours project.
    Remember to add the read more fuctonality on the tours info paragraphs.

--- src/2.Tours/Multipletours.jsx
+++ src/2.Tours/Multipletours.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-import Tour from "./Tour";
-const Tours = () => {
-  return <h2>tours component</h2>;
+const SingleTour = (props) => {
+  const tour = { ...props };
+
+  const { id, image, info, name, price, removeTour } = tour;
+  return (
+    <div key={id} className="single-tour">
+      <img src={image} alt="Tour Image" className="single-tour" />
+      <h4>{name}</h4>
+      <h4>{price}</h4>
+      <p className="tour-info">{info}</p>
+      <button className="delete-btn" onClick={() => removeTour(id)}>
+        Delete Tour
+      </button>
+    </div>
+  );
 };
 
-export default Tours;
+export default SingleTour;
--- src/2.Tours/Tour.jsx
+++ src/2.Tours/Tour.jsx
@@ -1,30 +1,39 @@
 import { useEffect, useState } from "react";
 import Loading from "./Loading";
 import IsError from "./IsError";
+import SingleTour from "./Multipletours";
 
 const Tour = () => {
   const url = "https://course-api.com/react-tours-project";
   const [error, setIsError] = useState(false);
   const [loading, setIsLoading] = useState(true);
   const [tours, setTours] = useState([]);
-  // const [singleTour, setSingleTour] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const allTours = await fetch(url);
-        const tourData = await allTours.json();
-        // console.log(tourData);
-        if (tourData) {
-          setTours(tourData);
-        }
-      } catch (error) {
-        console.log(error);
-        setIsError(true);
+
+  const fetchData = async () => {
+    try {
+      const allTours = await fetch(url);
+      const tourData = await allTours.json();
+      // console.log(tourData);
+      if (tourData) {
+        setTours(tourData);
       }
-      setIsLoading(false);
-    };
+    } catch (error) {
+      console.log(error);
+      setIsError(true);
+    }
+    setIsLoading(false);
+  };
+  useEffect(() => {
     fetchData();
   }, []);
+
+  const removeTour = (id) => {
+    const remainingTours = tours.filter((test) => {
+      return test.id != id;
+    });
+
+    setTours(remainingTours);
+  };
 
   if (loading) {
     return <Loading />;
@@ -32,24 +41,36 @@ const Tour = () => {
   if (error) {
     return <IsError />;
   }
+  if (tours.length === 0) {
+    return <Refresh fetchData={fetchData} />;
+  }
 
   return (
     <div>
-      {tours.map((myTour) => {
-        const { id, image, info, name, price } = myTour;
-        console.log(name);
-        console.log("Hello");
-        return (
-          <div key={id} className="single-tour">
-            <img src={image} alt="Tour Image" className="single-tour" />
-            <h4>{name}</h4>
-            <h4>{price}</h4>
-            <p className="tour-info">{info}</p>
-            <button className="delete-btn">Delete Tour</button>
-          </div>
-        );
-      })}
+      {notours ? (
+        <Refresh fetchData={fetchData} />
+      ) : (
+        tours.map((myTour) => {
+          return (
+            <div key={myTour.id}>
+              <SingleTour {...myTour} removeTour={removeTour} />
+            </div>
+          );
+        })
+      )}
     </div>
   );
 };
 export default Tour;
+
+export const Refresh = (props) => {
+  const func = { ...props };
+  const { fetchData } = func;
+  return (
+    <div>
+      <button className="delete-btn" onClick={() => fetchData()}>
+        Refresh
+      </button>
+    </div>
+  );
+};
