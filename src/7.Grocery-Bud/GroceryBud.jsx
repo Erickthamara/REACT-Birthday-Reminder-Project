de148265ff7dc2680438e0405db886aeae4c7b82
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2024-02-01 09:40:37 +0300

    Added the items and clear button.Also the delete button works for the items.

--- src/7.Grocery-Bud/GroceryBud.jsx
+++ src/7.Grocery-Bud/GroceryBud.jsx
@@ -2,22 +2,66 @@ import { useState } from "react";
 import { IoTrashBin } from "react-icons/io5";
 import { IoMdCheckmarkCircle } from "react-icons/io";
 const GroceryBud = () => {
-  const [grocerys, SetGrocerys] = useState([]);
+  const [grocerys, setGrocerys] = useState([]);
+  const [item, setItem] = useState("");
   const handleSubmit = (e) => {
-    e.preventdefault;
-    console.log(e.target.value);
+    e.preventDefault();
+    console.log(item);
+    const newGrocerys = [...grocerys, item];
+    setGrocerys(newGrocerys);
+    console.log(newGrocerys);
+  };
+  const handledeleteItem = (id) => {
+    const newList = grocerys.filter((item, index) => {
+      return index !== id;
+    });
+    setGrocerys(newList);
+  };
+  const handleClear = () => {
+    setGrocerys([]);
   };
   return (
     <section className="section-center">
+      {}
       <h3>Grocery Bud</h3>
       <form action="" className="grocery-form" onSubmit={handleSubmit}>
         <div className="form-control">
-          <input type="text" className="grocery" />
-          <button className="submit-btn">SUBMIT</button>
+          <input
+            type="text"
+            className="grocery"
+            onChange={(e) => setItem(e.target.value)}
+            placeholder="eg.milk"
+          />
+
+          <button className="submit-btn" type="submit">
+            SUBMIT
+          </button>
         </div>
       </form>
-
-      <div className="grocery-container">test</div>
+      {grocerys.length !== 0 &&
+        grocerys.map((foodItem, index) => {
+          return (
+            <div className="grocery-container" key={index}>
+              {" "}
+              <article className="grocery-item">
+                <p className="title">{foodItem}</p>
+                <button>
+                  <IoTrashBin onClick={() => handledeleteItem(index)} />
+                </button>
+                <button>
+                  <IoMdCheckmarkCircle
+                    onClick={() => console.log("hello world")}
+                  />
+                </button>
+              </article>
+            </div>
+          );
+        })}
+      {grocerys.length !== 0 && (
+        <button className="clear-btn" onClick={handleClear}>
+          CLEAR
+        </button>
+      )}
     </section>
   );
 };
